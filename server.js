1d5c1bbaddd44d87920ba2fc0d218bd63a0f19cf
pandeypearl <31654281+pandeypearl@users.noreply.github.com>
2022-12-30 00:08:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('user-db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || https://pandeypearl.github.io/users-api/user-db.json;
 
 server.use(middlewares);
 server.use(router);
